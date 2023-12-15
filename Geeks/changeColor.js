90e37762f1d5f12fdf59b0ffa889aedcbb55d7f6
pipjivko <pipjivko@gmail.com>
2023-12-16 01:54:16 +0200

    now the table colr is reset

--- Geeks/changeColor.js
+++ Geeks/changeColor.js
@@ -2,6 +2,12 @@ export default function changeColor(table){
 
     // table.pathMinSum.arr // [1,3,12,22]
     let allTd= document.getElementsByTagName('td')
+    console.log(allTd);
+    for (let index = 0; index < allTd.length; index++) {
+        const element = allTd[index];
+        element.style.backgroundColor ="aquamarine"
+        
+    }
     table.pathMinSum.arr.forEach(e => {
         allTd[e].style.backgroundColor = 'red'
     })
