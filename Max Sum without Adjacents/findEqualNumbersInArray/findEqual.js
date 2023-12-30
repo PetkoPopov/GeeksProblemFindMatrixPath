1f85924165a1b7d4a849c4445ae6cdb99d6d9ff9
pipjivko <pipjivko@gmail.com>
2023-12-30 21:31:07 +0200

    finaly decision withot functions

--- Max Sum without Adjacents/findEqualNumbersInArray/findEqual.js
+++ Max Sum without Adjacents/findEqualNumbersInArray/findEqual.js
@@ -1,40 +1,43 @@
 class Solution {
 
-    duplicates(a,n) {
+    duplicates(a, nn) {
         //7 ,6 ,8 ,4 ,9, 8, 4 ,7,4 ,4
-        // let n = a.length
+        let n = a.length
         let result = []
+        let count = -1
         for (let i = 0; i < n; i++) {
-            let element = a.pop()
-            if (a.includes(element)) {
-                result.push(element)
-                a = a.filter(el => {
-                    return el !== element
-                })
-            }
-        }
+            let hlp = a[n - i - 1]
+            a[n - 1 - i] = '@'
+            let isFound = false
+            for (let r = 0; r < n - i - 1; r++) {
+                if (a[r] === hlp && a[r] != '@' && hlp != '@') {
+                    a[r] = '@'
+                    if (!isFound) {
+                        count++
+                        let isSorted = false
+                        if (result.length == 0) { result[count] = hlp } else {
 
-        let resultSort = []
-        let ln = result.length
-        for (let r = 0; r < n; r++) {
-            let min = result[0]
-            let index = 0;
-            for (let i = 1; i < result.length; i++) {
-                if (min > result[i]) {
-                    min = result[i]
-                    index = i
+                            for (let p = 0; p < result.length; p++) {
+                                if (hlp < result[p]) {
+                                    for (let y = result.length; y >= p; y--) {
+                                        result[y] = result[y - 1]
+                                    }
+                                    result[p] = hlp
+                                    isSorted = true
+                                    break
+                                }
+                            }
+                        }
+                        if (!isSorted) { result[count] = hlp }
+                    }
+                    isFound = true
                 }
             }
-            // console.log('smallest index ', index, "smallest num ", min);
-            result.splice(index, 1)
-            // console.log(`result === > ${result}`);
-            resultSort.push(min)
-
-            if (resultSort.length == ln) break
         }
-        // resultSort.push(result[0])
-        return resultSort
+        if(result.length == 0){return -1}
+        else  {return result}
+
     }
 }
 let sol = new Solution()
-console.log(sol.duplicates([3, 4, 12, 3, 12, 3, 4, 4, 12, 7, 11, 6, 5, 99, 123, 123, 45, 43, 43, 55, 55, 55, 22, 22, 90, 45, 90, 90, 123, 1, 1, 1, 2, 12, 12, 3, 3], 13));
+console.log(sol.duplicates([3, 4, 12, 3, 0,0,1111,1111,12, 3,33,33,33,1111,33, 4, 4, 12, 7, 11, 6, 5, 99, 123, 123, 45, 43, 43, 55, 55, 55, 22, 22, 90, 45, 90, 90, 123, 1, 1, 1, 2, 12, 12, 3, 3], 13));  
